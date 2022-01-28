3ad584406cd43b16351bb5889c68d6d11a5af663
Fidomi <sidonie.moulart@gmail.com>
2022-01-28 14:04:45 +0100

    Add a controller to the model

--- src/controllers/jasonControllers.js
+++ src/controllers/jasonControllers.js
@@ -3,30 +3,77 @@ import { ApplicantSchema } from "../models/jasonModel";
 
 const Applicant = mongoose.model("Applicant", ApplicantSchema);
 
-export const addNewApplicant = (req, res) => {
-    let newApplicant = new Applicant(req.body);
-    newApplicant.save((err, applicant) => {
-        if (err) {
-            res.send(err);
+export const getAllApplicants = async () => {
+    try {
+        const allApplicants = await Applicant.find({});
+        if (!allApplicants) {
+            throw new Error("Impossible");
+        } else {
+            return allApplicants;
         }
-        res.json(applicant);
-    });
+    } catch (err) {
+        return err;
+    }
 };
 
-export const getApplicants = (req, res) => {
-    Applicant.find({}, (err, applicant) => {
-        if (err) {
-            res.send(err);
+export const getApplicantWithID = async (id) => {
+    try {
+        const applicant = await Applicant.findById(id).exec();
+        if (!applicant) {
+            throw new Error("Cet id ne correspond à aucun candidat");
+        } else {
+            return applicant;
         }
-        res.json(applicant);
-    });
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
 };
 
-export const getApplicantWithID = (req, res) => {
-    Applicant.findById(req.params.applicantID, (err, applicant) => {
-        if (err) {
-            res.send(err);
+export const findApplicantByName = async (name) => {
+    try {
+        if (name) {
+            const applicant = await Applicant.findOne({
+                name: name,
+            }).exec();
+            if (!applicant) {
+                return false;
+            } else {
+                return applicant;
+            }
+        } else {
+            throw new Error("Il manque le nom du candidat.");
         }
-        res.json(applicant);
-    });
+    } catch (err) {
+        console.error(err.message);
+        return err.message;
+    }
+};
+
+export const addNewApplicant = async (name, age, height, weight, sport) => {
+    try {
+        const alreadyExists = await findApplicantByName(name);
+        if (!alreadyExists) {
+            let newApplicant = new Applicant({
+                name: name,
+                age: age,
+                height: height,
+                weight: weight,
+                sport: sport,
+            });
+            const result = await newApplicant.save();
+            if (!result) {
+                throw new Error(
+                    "Impossible d'enregistrer ce nouvel utilisateur."
+                );
+            } else {
+                return result;
+            }
+        } else {
+            throw new Error("Cet utilisateur existe déjà.");
+        }
+    } catch (err) {
+        console.error(err.message);
+        return err.message;
+    }
 };
