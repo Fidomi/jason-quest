3be55042ff18934ccaca2e5145aed6a0e1187c79
Fidomi <sidonie.moulart@gmail.com>
2022-01-28 17:29:36 +0100

    Add Style

---
+++ frontend/src/utils/style/fonts.js
@@ -0,0 +1,14 @@
+export const fonts = {
+    fontLogo: `'Bubbler One', sans-serif;`,
+    fontTitle: `'Playfair Display', serif`,
+    fontBody: `'Roboto', sans-serif`,
+};
+export const fontSizes = {
+    fontXS: `12px`,
+    fontS: `14px`,
+    fontM: `16px`,
+    fontRegular: `18px`,
+    fontL: `24px`,
+    fontXL: `32px`,
+    fontXXL: `48px`,
+};
